c5fa6ba2705a9171e19d1be31875c0211aeeaffc
zbychhhu <grzegorzmarchewka351@gmail.com>
2019-04-15 17:15:14 +0200

    login

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CarList from "./CarList";
 import CarEdit from "./CarEdit";
+import { CookiesProvider } from "react-cookie";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/cars" exact={true} component={CarList} />
-          <Route path="/cars/:id" component={CarEdit} />
-        </Switch>
-      </Router>
+      <CookiesProvider>
+        <Router>
+          <Switch>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/cars" exact={true} component={CarList} />
+            <Route path="/cars/:id" component={CarEdit} />
+          </Switch>
+        </Router>
+      </CookiesProvider>
     );
   }
 }
